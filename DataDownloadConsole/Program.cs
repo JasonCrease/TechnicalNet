cd1a93dfbdb0c2e18d4bef872f5b8e9c97853e29
Jason Crease <jasoncrease@gmail.com>
2014-09-20 21:42:40 +0100

    data downloader created

---
+++ DataDownloadConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDownloadConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // http://ichart.finance.yahoo.com/table.csv?d=6&e=1&f=2009&g=d&a=7&b=19&c=2004&ignore=.csv&s=
+            // http://www.google.com/finance/historical?output=csv&q=[Symbol name]
+        }
+    }
+}
