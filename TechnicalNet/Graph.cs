9636d5216186f570ba0d7ebf9e4487e2e6098c09
Jason Crease <jasoncrease@gmail.com>
2014-09-25 22:46:33 +0100

    Made graph scalable and better written

--- TechnicalNet/Graph.cs
+++ TechnicalNet/Graph.cs
@@ -17,14 +17,23 @@ namespace TechnicalNet
         Graphics m_Graphics;
         TechnicalNet.StockHistory m_StockHistory;
         List<TechnicalNet.Metrics.IMetric> m_Metrics;
-        int GraphStartX = 50;
-        int GraphStartY = 20;
+
+        int MarginHorizontal = 50;
+        int MarginVertical = 20;
+
+        int BitmapHeight = 600;
+        int BitmapWidth = 900;
+
+        int GraphHeight = -1;
+        int GraphWidth = -1;
+
+        double HorizontalStretch = -1;
 
         public Graph(TechnicalNet.StockHistory stockHistory, List<TechnicalNet.Metrics.IMetric> metrics)
         {
             m_StockHistory = stockHistory;
             m_Metrics = metrics;
-            Bitmap = new Bitmap(700, 600);
+            Bitmap = new Bitmap(BitmapWidth, BitmapHeight);
             m_Graphics = Graphics.FromImage(Bitmap);
 
             yMin = stockHistory.Opens.Min();
@@ -32,6 +41,11 @@ namespace TechnicalNet
             xMin = stockHistory.StartDate.Ticks;
             xMax = stockHistory.EndDate.Ticks;
             yRange = yMax - yMin;
+
+            GraphHeight = BitmapHeight - (MarginVertical * 2);
+            GraphWidth = BitmapWidth - (MarginHorizontal * 2);
+
+            HorizontalStretch = ((double)GraphWidth / 250D);
 
             DrawLeftAxis();
             DrawBottomAxis();
@@ -54,14 +68,14 @@ namespace TechnicalNet
             Pen penLightBlue = new Pen(Color.LightBlue);
             double y = 0d;
             double x = 0d;
-            
+
             for (int i = 0; i < m_StockHistory.Count; i++)
             {
                 double val = m_StockHistory.Opens[i];
                 double oldy = y;
                 double oldx = x;
-                y = (((yMax - val) / yRange) * 460d) + GraphStartY;
-                x = (i * 2) + GraphStartX;
+                y = (((yMax - val) / yRange) * GraphHeight) + MarginVertical;
+                x = (i * HorizontalStretch) + MarginHorizontal;
 
                 //m_Graphics.DrawEllipse(penBlue, (int)x, (int)y, 2, 2);
 
@@ -75,32 +89,32 @@ namespace TechnicalNet
         private void DrawLeftAxis()
         {
             Pen p = new Pen(Color.Black);
-            m_Graphics.DrawLine(p, GraphStartX, GraphStartY, GraphStartX, 480);
-            m_Graphics.DrawString(yMin.ToString(), new Font(FontFamily.GenericSerif, 6), Brushes.BlueViolet, 5, 460);
-            m_Graphics.DrawString(yMax.ToString(), new Font(FontFamily.GenericSerif, 6), Brushes.BlueViolet, 5, 10);
+            m_Graphics.DrawLine(p, MarginHorizontal, MarginVertical, MarginHorizontal, GraphHeight + MarginVertical);
+            m_Graphics.DrawString(yMax.ToString(), new Font(FontFamily.GenericSerif, 9), Brushes.BlueViolet, 5, MarginVertical);
+            m_Graphics.DrawString(yMin.ToString(), new Font(FontFamily.GenericSerif, 9), Brushes.BlueViolet, 5, GraphHeight + MarginVertical);
         }
 
         private void DrawCutoff()
         {
             Pen p = new Pen(Color.Gray);
-            m_Graphics.DrawLine(p, GraphStartX + 300, 20, GraphStartX + 300, 480);
+            m_Graphics.DrawLine(p, MarginHorizontal + (int)(HorizontalStretch * 150), MarginVertical, MarginHorizontal + (int)(HorizontalStretch * 150), GraphHeight - MarginVertical);
         }
 
         private void DrawBottomAxis()
         {
             Pen p = new Pen(Color.Black);
-            m_Graphics.DrawLine(p, GraphStartX, 480, 500 + GraphStartX, 480);
+            m_Graphics.DrawLine(p, MarginHorizontal, GraphHeight + MarginVertical, GraphWidth + MarginHorizontal, GraphHeight + MarginVertical);
             m_Graphics.DrawString(m_StockHistory.StartDate.ToShortDateString(),
-                new Font(FontFamily.GenericSansSerif, 6), Brushes.BlueViolet, GraphStartX - 30, 480);
+                new Font(FontFamily.GenericSansSerif, 9), Brushes.BlueViolet, MarginHorizontal, GraphHeight + MarginVertical);
             m_Graphics.DrawString(m_StockHistory.EndDate.ToShortDateString(),
-                new Font(FontFamily.GenericSansSerif, 6), Brushes.BlueViolet, 500 + GraphStartX - 30, 480);
+                new Font(FontFamily.GenericSansSerif, 9), Brushes.BlueViolet, GraphWidth + MarginHorizontal - 20, GraphHeight + MarginVertical);
         }
 
         internal void DrawPoint(Color color, double t, double val)
         {
             Pen p = new Pen(color);
-            double y = (((yMax - val) / yRange) * 460d) + GraphStartY;
-            double x = (t * 2) + GraphStartX;
+            double y = (((yMax - val) / yRange) * GraphWidth) + MarginVertical;
+            double x = (t * HorizontalStretch) + MarginHorizontal;
             m_Graphics.DrawRectangle(p, (int)x, (int)(y), 1, 1);
         }
 
@@ -116,8 +130,8 @@ namespace TechnicalNet
                 double val = ys[i];
                 double oldy = y;
                 double oldx = x;
-                y = (((yMax - val) / yRange) * 460d) + GraphStartY;
-                x = (i * 2) + GraphStartX;
+                y = (((yMax - val) / yRange) * GraphHeight) + MarginVertical;
+                x = (i * HorizontalStretch) + MarginHorizontal;
 
                 if (i > 0 && y > 1 && oldy > 1)
                 {
