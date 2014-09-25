7f42e6860c34c8fa70eaaadd3eb5333e48f896d3
Jason Crease <jason.crease@red-gate.com>
2014-09-25 21:45:10 +0100

    starting to implement adx

--- TechnicalNet/Predictors/AdxPredictor.cs
+++ TechnicalNet/Predictors/AdxPredictor.cs
@@ -15,7 +15,23 @@ namespace TechnicalNet.Predictors
 
         public void Analyse(StockHistory stock)
         {
-            throw new NotImplementedException();
+            double dmplus = 0;
+            double dmminus = 0;
+            double alpha = 0.1;
+
+            for(int i=150; i>50; i--)
+            {
+                double upmove = stock.Highs[i] - stock.Highs[i - 1];
+                double downmove = stock.Lows[i-1] - stock.Lows[i];
+                if (upmove > downmove && upmove > 0)
+                    dmplus = alpha*upmove + (1-alpha) * dmplus;
+                else dmplus = 0;
+                if (downmove > upmove && downmove > 0)
+                    dmminus = alpha * downmove + (1 - alpha) * dmminus;
+            }
+
+            double diplus = (100 * dmplus) / stock.ATR[150];
+            double diminus = (100 * dmplus) / stock.ATR[150];
         }
     }
 }
