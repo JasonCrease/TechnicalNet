60fe8e30de03bffc992c6a19e897d302007879cf
Jason Crease <jason.crease@red-gate.com>
2014-09-26 17:58:09 +0100

    added evaluator

---
+++ TechnicalNet/Strategy/Evaluator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalNet.Strategy
+{
+    class Evaluator
+    {
+        /// <summary>
+        /// How good the strategy is. Specifically, if you invested $1 in it, how much would you get back.
+        /// </summary>
+        /// <returns>The wins/loses on 1.00 dollars</returns>
+        public double GetOutcome()
+        {
+            return 1;
+        }
+    }
+}
