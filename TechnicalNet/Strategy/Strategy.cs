9aeea1f3747c535911679ca3849e62d091772372
Jason Crease <jasoncrease@gmail.com>
2014-09-28 12:01:17 +0100

    moved strategy around

---
+++ TechnicalNet/Strategy/Strategy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalNet.RealData;
+
+namespace TechnicalNet.Strategy
+{
+    public abstract class Strategy
+    {
+        public abstract double PredictValue(TechnicalNet.RealData.StockHistory stockHistory, int today, int daysInFuture);     
+        
+        /// <summary>
+        /// How good the strategy is. Specifically, if you invested $1 in it, how much would you get back.
+        /// </summary>
+        /// <returns>The wins/loses on 1.00 dollars</returns>
+        public double GetStrategyProfit(StockHistorySet testData, int today, int daysToPredict)
+        {
+            double profit = 0D;
+
+            foreach (var stock in testData.AllStocks)
+            {
+                double predictedValue = this.PredictValue(stock, 150, 50);
+                double actualValue = stock.Closes[today + daysToPredict];
+                profit += (actualValue - predictedValue);
+            }
+
+            return profit;
+        }
+    }
+}
--- TechnicalNet/Strategy/ThingsDontChangeStrategy.cs
+++ TechnicalNet/Strategy/ThingsDontChangeStrategy.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 namespace TechnicalNet.Strategy
 {
     // Just return today's value
-    public class ThingsDontChangeStrategy : IStrategy
+    public class ThingsDontChangeStrategy : Strategy
     {
-        public double PredictValue(TechnicalNet.RealData.StockHistory stockHistory, int today, int daysInFuture)
+        public override double PredictValue(TechnicalNet.RealData.StockHistory stockHistory, int today, int daysInFuture)
         {
             return stockHistory.Closes[today];
         }
